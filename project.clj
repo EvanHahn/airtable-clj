71ed41ac0034b11f3f84e5a4f16afe0ad3d2e6a3
Evan Hahn <me@evanhahn.com>
2018-03-28 15:33:26 +0000

    Update some project.clj metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject airtable-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :description "A Clojure client for the Airtable API."
+  :url "https://github.com/EvanHahn/airtable-clj"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.8.0"]])
